7c31d00cd9c2c7ec03b9fec9ee8f07d0444ebd7e
Norberto Aguirre <njaguirre@gmail.com>
2025-05-28 12:22:14 -0300

    first commit

---
+++ src/test/java/ar/com/southernsoft/service/client/MainResourceIT.java
@@ -0,0 +1,8 @@
+package ar.com.southernsoft.service.client;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class MainResourceIT extends MainResourceTest {
+    // Execute the same tests but in packaged mode.
+}
